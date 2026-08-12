a2cbdf961acec1451f8ffe3074c2fb54f5575bb2
Isa Conrado <isafconradosilva@gmail.com>
2026-08-12 15:37:10 -0300

    Adiciona funcao de adicionar nota a lista de notas

--- notas/bin/notas.dart
+++ notas/bin/notas.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 void main () {
   getComando();
+  adicionaNota(notas);
 }
 
 void getComando() {
@@ -16,4 +17,21 @@ void getComando() {
     getComando();
   }
 
+}
+
+List<String> notas = [];
+
+List<String> adicionaNota(List<String> notas) {
+  String? nota = "";
+  print("Adicione uma nota:");
+  nota = stdin.readLineSync();
+
+  if (nota == null || nota.isEmpty) {
+    print("Não é possível adicionar uma nota vazia.");
+    adicionaNota(notas);
+  } 
+  
+  notas.add(nota!);
+
+  return notas;
 }
